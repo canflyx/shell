adf849fa558d986989a6d26bf84d37450eeb423e
canflyx <168555@gmail.com>
2020-11-11 14:03:46 +0800

    add it

--- autox.sh
+++ autox.sh
@@ -9,10 +9,36 @@
 # check curl
 
 # install fping
+# check curl fping is installed
+
+if [[ -f /etc/redhat-release ]]; then
+        release="centos"
+        yum install curl fping -y
+elif cat /etc/issue | grep -q -E -i "debian"; then
+        release="debian"
+        apt install curl fping -y
+elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+        apt install curl fping -y
+elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+        yum install curl fping -y
+elif cat /proc/version | grep -q -E -i "debian"; then
+        release="debian"
+        apt install curl fping -y
+elif cat /proc/version | grep -q -E -i "ubuntu"; then
+        release="ubuntu"
+        apt install curl fping -y
+
+elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+        release="centos"
+        yum install curl fping -y
+fi
 
 #2.---------program------------
 #time=日志时间戳，workDir=工作目录，v2Dir=v2目录，defaultSpeed=默认需要达到的速度，num=ping测试次数，interval=ping中间间隔,saveIp=ping测试后保留的IP地址数量。测试后速度快的IP地址保留15个
 #对IP进行测试10S并输出速度
+
 workDir=/tmp/cf
 v2Dir=/etc/v2ray
 defaultSpeed=70
@@ -22,7 +48,11 @@ saveIp=100
 v2Ip=$(sed -n '/address/'p $v2Dir/config.json | awk -F "\"" '{print $4}')
 testUrl=https://apple.freecdn.workers.dev/105/media/us/iphone-11-pro/2019/3bd902e4-0752-4ac1-95f8-6225c32aec6d/films/product/iphone-11-pro-product-tpl-cc-us-2019_1280x720h.mp4
 testDoman=apple.freecdn.workers.dev
-
+if test -s v2Dir/config.json; then
+    v2Ip=$(sed -n '/address/'p $v2Dir/config.json | awk -F "\"" '{print $4}')
+else
+    echo "V2 directory does not exist! please modify!"
+fi
 function UrlTest() {
     echo $(curl -w %{speed_download} --resolve $testDomain:$1 $testUrl -o /dev/null -s --connect-timeout 2 --max-time 10) / 131072.000 | bc
 }
